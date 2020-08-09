54d901c0638b61a5955dc9e735e37edac77b5bf8
Timothy Samraj <tsam0005@student.monash.edu>
2020-08-09 12:45:20 +1000

    Simplify video creation flow

--- screenlapse-convert.py
+++ screenlapse-convert.py
@@ -6,7 +6,6 @@ import cv2
 
 
 def main():
-    img_list = []
     files = sorted(
         glob.glob(str(pathlib.Path().absolute()) + "\\img\\*"), key=os.path.getmtime
     )
@@ -15,13 +14,10 @@ def main():
     height, width, _ = test_img.shape
     size = (width, height)
 
-    print("Processing " + str(len(files)) + " images...")
-    for file in files:
-        img_list.append(cv2.imread(file))
+    print("Processing " + str(len(files)) + " images and creating video...")
     out = cv2.VideoWriter("screenlapse.mp4", cv2.VideoWriter_fourcc(*"mp4v"), 10, size)
-    print("Creating video...")
-    for i in range(len(img_list)):
-        out.write(img_list[i])
+    for file in files:
+        out.write(cv2.imread(file))
     print("Finishing up...")
     out.release()
     print("Done!")
