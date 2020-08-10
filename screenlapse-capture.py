139e96205970b8f3154ff71572a53c4c7aea05d3
Timothy Samraj <tsam0005@student.monash.edu>
2020-08-10 11:58:29 +1000

    Add offline mode for image capture

--- screenlapse-capture.py
+++ screenlapse-capture.py
@@ -1,4 +1,5 @@
 import argparse
+import os
 import subprocess
 import time
 
@@ -18,7 +19,7 @@ def push_img():
     )
 
 
-def handle_args() -> int:
+def handle_args() -> argparse.Namespace:
     parser = argparse.ArgumentParser()
     parser.add_argument(
         "-t",
@@ -27,26 +28,46 @@ def handle_args() -> int:
         type=int,
         metavar="secs",
     )
+    parser.add_argument(
+        "-o",
+        "--offline",
+        help="Use filesystem for image storage instead of S3",
+        action="store_true",
+    )
     args = parser.parse_args()
     if args.time is None:
         args.time = 3
     if args.time <= 0:
         raise argparse.ArgumentTypeError("Time must be positive")
-    return args.time
+    return args
 
 
 def main():
-    sleep_time = handle_args()
+    args = handle_args()
+    sleep_time = args.time
+    offline = args.offline
     actions.set_session_key()
+    if offline:
+        try:
+            os.mkdir("img")
+        except FileExistsError:
+            print("img directory already exists. Exiting...")
+            exit()
     i = 1
     while True:
-        # grab screenshot
-        # push to aws
-        # wait 5 secs
-        pyautogui.screenshot(constants.IMG_NAME)
-        push_img()
-        subprocess.run(["cls"], shell=True)
-        print("Image " + str(i) + " captured and pushed. [ctrl+c to quit]")
+        if not offline:
+            pyautogui.screenshot(constants.IMG_NAME)
+            push_img()
+            subprocess.run(["cls"], shell=True)
+            print("Image " + str(i) + " captured and pushed. [ctrl+c to quit]")
+        else:
+            pyautogui.screenshot("img/img" + str(i) + ".png")
+            subprocess.run(["cls"], shell=True)
+            print(
+                "Image "
+                + str(i)
+                + " captured. Running in offline mode. [ctrl+c to quit]"
+            )
         i += 1
         time.sleep(sleep_time)
 
